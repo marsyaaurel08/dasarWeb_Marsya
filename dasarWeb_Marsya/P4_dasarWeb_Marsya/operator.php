376a73f33cea2ce41dc13814809ba53a4a125405
marsyaaurel08 <sefiramarsya@gmail.com>
2024-09-17 09:26:28 +0700

    P4_PraktikumBag3_Langkah2_soal3.1_membuat file operator.php, serta merapikan untuk hasilnya saat muncul di browser

---
+++ dasarWeb_Marsya/P4_dasarWeb_Marsya/operator.php
@@ -0,0 +1,28 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Bilangan 1= {$a} <br>";
+echo "Bilangan 2= {$b} <br>";
+echo "<br>";
+echo "Hasil Tambah: {$hasilTambah}" ;
+echo "<br>";
+echo "Hasil Kurang: {$hasilKurang}" ;
+echo "<br>";
+echo "Hasil Kali: {$hasilKali}" ;
+echo "<br>";
+echo "Hasil Bagi: {$hasilBagi}" ;
+echo "<br>";
+echo "Sisa Bagi: {$sisaBagi}" ;
+echo "<br>";
+echo "Hasil Pangkat: {$pangkat}" ;
+echo "<br>";
+
+?>
