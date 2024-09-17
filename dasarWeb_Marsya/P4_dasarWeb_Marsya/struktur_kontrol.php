040dec15558b9a9585da64e2aa0835f88991449a
marsyaaurel08 <sefiramarsya@gmail.com>
2024-09-17 11:51:58 +0700

    P4_PraktikumBag4_Langkah6_soal 4.2  menambah isi pada file struktur_kontrol.php

--- dasarWeb_Marsya/P4_dasarWeb_Marsya/struktur_kontrol.php
+++ dasarWeb_Marsya/P4_dasarWeb_Marsya/struktur_kontrol.php
@@ -10,4 +10,17 @@ echo "Nilai huruf: C";
 } elseif ($nilaiNumerik < 70) {
 echo "Nilai huruf: D";
 }
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "<br> Atlet tersebut memerlukan $hari hari untuk mencapai jarak
+500 kilometer."
 ?>
