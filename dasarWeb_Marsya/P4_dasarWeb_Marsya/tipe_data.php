c2cfcbe0cf774cc75e3a1e0731075ba4dd351eea
marsyaaurel08 <sefiramarsya@gmail.com>
2024-09-17 08:55:15 +0700

    P4_PraktikumBag2_Langkah5_ menambah variabel nilai dan rata-rata pada file tipe_data.php

--- dasarWeb_Marsya/P4_dasarWeb_Marsya/tipe_data.php
+++ dasarWeb_Marsya/P4_dasarWeb_Marsya/tipe_data.php
@@ -13,4 +13,17 @@ echo "Variabel e: {$e} <br>";
 
 var_dump($e);
 
+$nilaiMatematika = 5.1;
+$niaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
+
 ?>
