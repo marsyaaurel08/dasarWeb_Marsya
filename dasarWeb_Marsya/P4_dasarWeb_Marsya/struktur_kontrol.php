78cf87c779762c5ab33f354f1b3db13fa0fab5da
marsyaaurel08 <sefiramarsya@gmail.com>
2024-09-17 12:17:56 +0700

    P4_PraktikumBag4_Langkah18_ menambah array list nilai siswa pada file struktur_kontrol.php

--- dasarWeb_Marsya/P4_dasarWeb_Marsya/struktur_kontrol.php
+++ dasarWeb_Marsya/P4_dasarWeb_Marsya/struktur_kontrol.php
@@ -42,5 +42,15 @@ foreach ($skorUjian as $skor) {
     $totalSkor += $skor;
 }
 
-echo "<br><br>Total skor ujian adalah: $totalSkor";
+echo "<br><br>Total skor ujian adalah: $totalSkor <br><br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (Lulus) <br>";
+}
 ?>
