87a78d88a5297f2b52bf2d8a4a2238dbba0f3f0d
marsyaaurel08 <sefiramarsya@gmail.com>
2024-09-17 09:36:14 +0700

    P4_PraktikumBag3_Langkah5_soal 3.2_menambah isi pada file operator.php

--- dasarWeb_Marsya/P4_dasarWeb_Marsya/operator.php
+++ dasarWeb_Marsya/P4_dasarWeb_Marsya/operator.php
@@ -23,6 +23,26 @@ echo "<br>";
 echo "Sisa Bagi: {$sisaBagi}" ;
 echo "<br>";
 echo "Hasil Pangkat: {$pangkat}" ;
+echo "<br><br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil Sama: {$hasilSama}" ;
+echo "<br>";
+echo "Hasil Tidak Sama: {$hasilTidakSama}" ;
+echo "<br>";
+echo "Hasil Lebih Kecil: {$hasilLebihKecil}" ;
+echo "<br>";
+echo "Hasil Lebih Besar: {$hasilLebihBesar}" ;
+echo "<br>";
+echo "Hasil Lebih Kecil Sama: {$hasilLebihKecilSama}" ;
+echo "<br>";
+echo "Hasil Lebih Besar Sama: {$hasilLebihBesarSama}" ;
 echo "<br>";
 
 ?>
