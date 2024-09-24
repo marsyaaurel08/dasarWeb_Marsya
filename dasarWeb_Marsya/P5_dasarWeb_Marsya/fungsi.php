0753a36fc1d95733174005397c150adc60cc9971
marsyaaurel08 <sefiramarsya@gmail.com>
2024-09-24 07:48:37 +0700

    P5_PHP02_PraktikumFungsiBerparameter_Langkah4_Soal2

--- dasarWeb_Marsya/P5_dasarWeb_Marsya/fungsi.php
+++ dasarWeb_Marsya/P5_dasarWeb_Marsya/fungsi.php
@@ -1,14 +1,23 @@
 <?php
 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Marsya<br/>";
+//membuat fungsi
+function perkenalan($nama, $salam){
+    echo  $salam. ", ";
+    echo "Perkenalkan, nama saya ". $nama. "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Aurelia","Hallo");
+
+echo "<hr>";
+
+$saya = "Marsya";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
+
 
 
 ?>
