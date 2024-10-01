685186e5d24d2902410a4736a47d4788f352fe93
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-01 09:23:59 +0700

    P6_PraktikumBag Fungsi Rekursif _ soal no 9 dan soal 10

--- dasarWeb_Marsya/P6_dasarWeb_Marsya/fungsi.php
+++ dasarWeb_Marsya/P6_dasarWeb_Marsya/fungsi.php
@@ -61,20 +61,22 @@
 
 //FUNGSI YANG DIPANGGIL DIDALAM FUNGSI 
 //membuat fungsi
-function hitungUmur($thn_lahir, $thn_sekarang){
-    $umur = $thn_sekarang - $thn_lahir;
-    return $umur;
+//function hitungUmur($thn_lahir, $thn_sekarang){
+  //  $umur = $thn_sekarang - $thn_lahir;
+    //return $umur;
 
-}
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo  $salam. ", ";
-    echo "Perkenalkan, nama saya ". $nama. "<br/>";
+//}
+//function perkenalan($nama, $salam="Assalamualaikum"){
+  //  echo  $salam. ", ";
+   // echo "Perkenalkan, nama saya ". $nama. "<br/>";
 
     //memanggil fungsi lain
-    echo "Saya berusia ". hitungUmur(2005, 2024) ." tahun<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
-}
+    //echo "Saya berusia ". hitungUmur(2005, 2024) ." tahun<br/>";
+    //echo "Senang berkenalan dengan Anda<br/>";
+//}
 
 //memanggil fungsi perkenalan
-perkenalan ("Marsya");
+//perkenalan ("Marsya");
+
+
 ?>
---
+++ dasarWeb_Marsya/P6_dasarWeb_Marsya/rekursif.php
@@ -0,0 +1,14 @@
+<?php
+//FUNGSI REKURSIF
+//function tampilkanHaloDunia(){
+  //  echo "Halo Dunia! <br>";
+    
+  //  tampilkanHaloDunia();
+//}
+
+//tampilkanHaloDunia();
+
+for ($i=1; $i <= 25; $i++){
+    echo "Perulangan ke-{$i} <br>";
+}
+?>
