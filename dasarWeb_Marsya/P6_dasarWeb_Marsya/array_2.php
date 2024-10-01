6a34c72eee394f0f0edbd1d37f30311e6bc532d8
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-01 08:27:44 +0700

    P6_PraktikumBag2_Associative Array_Soal 2

---
+++ dasarWeb_Marsya/P6_dasarWeb_Marsya/array_2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styleTabel.css"/>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <h1>Informasi Dosen</h1>
+        <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis_kelamin' => 'Perempuan'
+        ];
+        ?>
+
+        <table>
+            <tr>
+                <th>Field</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $Dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
+    </body>
+</html>
