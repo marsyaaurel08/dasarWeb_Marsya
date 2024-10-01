450aae0e99244e6203d10abeb8bd11bbd2a18d43
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-01 08:57:38 +0700

    P6_PraktikumBagFungsiYang Dipanggil didalam Fungsi_ Soal 8

--- dasarWeb_Marsya/P6_dasarWeb_Marsya/fungsi.php
+++ dasarWeb_Marsya/P6_dasarWeb_Marsya/fungsi.php
@@ -51,11 +51,30 @@
 
 //FUNGSI YANG MENGEMBALIKAN NILAI
 //membuat fungsi
+//function hitungUmur($thn_lahir, $thn_sekarang){
+  //  $umur = $thn_sekarang - $thn_lahir;
+    //return $umur;
+
+//}
+
+//echo "Umur saya adalah ". hitungUmur(2005, 2024). " tahun" 
+
+//FUNGSI YANG DIPANGGIL DIDALAM FUNGSI 
+//membuat fungsi
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 
 }
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo  $salam. ", ";
+    echo "Perkenalkan, nama saya ". $nama. "<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2005, 2024). " tahun" 
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2005, 2024) ." tahun<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi perkenalan
+perkenalan ("Marsya");
 ?>
