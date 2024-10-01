fa09d90b75eb2d947edbe1d5c9197a5e2453e4c1
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-01 08:44:03 +0700

    P6_PraktikumBagFungsi_Soal 4

---
+++ dasarWeb_Marsya/P6_dasarWeb_Marsya/fungsi.php
@@ -0,0 +1,15 @@
+<?php
+
+//membuat fungsi
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Marsya<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+
+
+?>
