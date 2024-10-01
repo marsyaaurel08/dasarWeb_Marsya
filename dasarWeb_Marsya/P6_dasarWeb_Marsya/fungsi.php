c1189d15834a30dd1bc01f9591646ddd36ce7aab
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-01 08:49:11 +0700

    P6_PraktikumBagFungsidengan parameter nilai default_soal 6

--- dasarWeb_Marsya/P6_dasarWeb_Marsya/fungsi.php
+++ dasarWeb_Marsya/P6_dasarWeb_Marsya/fungsi.php
@@ -1,15 +1,51 @@
 <?php
 
 //membuat fungsi
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Marsya<br/>";
+//function perkenalan(){
+//    echo "Assalamualaikum, ";
+  //  echo "Perkenalkan, nama saya Marsya<br/>";
+    //echo "Senang berkenalan dengan Anda<br/>";
+//}
+
+//memanggil fungsi yang sudah dibuat
+//perkenalan();
+//perkenalan();
+
+//FUNGSI BERPARAMETER
+//membuat fungsi
+//function perkenalan($nama, $salam){
+   // echo  $salam. ", ";
+    //echo "Perkenalkan, nama saya ". $nama. "<br/>";
+    //echo "Senang berkenalan dengan Anda<br/>";
+//}
+
+//memanggil fungsi yang sudah dibuat
+//perkenalan("Aurelia","Hallo");
+
+//echo "<hr>";
+
+//$saya = "Marsya";
+//$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi 
+//perkenalan($saya, $ucapanSalam);
+
+//FUNGSI DENGAN NILAI DEFAULT
+//membuat fungsi
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo  $salam. ", ";
+    echo "Perkenalkan, nama saya ". $nama. "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Aurelia","Hallo");
 
+echo "<hr>";
 
+$saya = "Marsya";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
 ?>
