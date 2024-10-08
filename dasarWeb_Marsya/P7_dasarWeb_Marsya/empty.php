1ecd42223b3f6224582a9339cb2b89d5a8fc4867
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-08 07:55:28 +0700

    P7_PraktikumBag2_FunctionEmpty_Langkah 4 _ Soal 2.2

--- dasarWeb_Marsya/P7_dasarWeb_Marsya/empty.php
+++ dasarWeb_Marsya/P7_dasarWeb_Marsya/empty.php
@@ -6,4 +6,10 @@ if (empty($myArray)) {
 } else {
     echo "Array terdefinisi dan tidak kosong.";
 }
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
 ?>
