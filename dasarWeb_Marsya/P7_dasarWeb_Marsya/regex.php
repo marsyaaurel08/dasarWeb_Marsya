ec939b7078093539d4d56537dc6d160a25acdf02
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-10 20:02:55 +0700

    P7_PraktikumBag5_Langkah 10_Soal 5.3

--- dasarWeb_Marsya/P7_dasarWeb_Marsya/regex.php
+++ dasarWeb_Marsya/P7_dasarWeb_Marsya/regex.php
@@ -18,4 +18,12 @@ if (preg_match($pattern, $text, $matches)) {
 } else {
     echo "Tidak ada yang cocok";
 }
+
+echo "<br><br>";
+
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output : "I like banana pie."
 ?>
